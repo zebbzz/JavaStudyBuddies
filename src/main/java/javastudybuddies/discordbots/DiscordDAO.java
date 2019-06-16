c21018d1f5dd8ae2f105de8d83a64ae9a69d932f
stormstricker <stormster@tutanota.com>
2019-06-16 22:06:37 +0300

    gitignore file

--- src/main/java/javastudybuddies/discordbots/DiscordDAO.java
+++ src/main/java/javastudybuddies/discordbots/DiscordDAO.java
@@ -1,8 +1,11 @@
 package javastudybuddies.discordbots;
 
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DiscordDAO {
    private static Connection connection;
@@ -13,10 +16,34 @@ public class DiscordDAO {
    }
 
     public static void main(String[] args) {
-       DiscordUser user = new DiscordUser();
+       /*DiscordUser user = new DiscordUser();
        user.setName("Test");
-       user.setLevel("Beginner");
-        insert(user);
+       user.setLevel("Advanced");
+       user.setAge(24);
+       user.setCountry("Brazil");
+       user.setTimezone("UTC+3");
+        insert(user);*/
+
+       /*DiscordUser user = new DiscordUser();
+       user.setName("copperlark2");
+       int id = DiscordDAO.getId(user);
+       System.out.println("id: " + id);
+
+       DiscordUser user1 = DiscordDAO.getById(DiscordUser.class, 3);
+       System.out.println(user1.getName());
+
+       DiscordUser user2 = DiscordDAO.getByUsername(DiscordUser.class, "copperlark2");
+       System.out.println("username: " + user2.getName());*/
+
+       /* DiscordUser user3 = DiscordDAO.getByUsername(DiscordUser.class, "copperlark");
+        for (String question : user3.getAnswered().keySet()) {
+            System.out.println(question + ": " + user3.getAnswered().get(question).getAnswer());
+        }*/
+
+        DiscordUser user4 = DiscordDAO.getByUsername(DiscordUser.class, "copperlark ");
+        for (String question : user4.getAnswered().keySet()) {
+            System.out.println(question + ": " + user4.getAnswered().get(question).getAnswer());
+        }
     }
 
     private static boolean connect(String DB_URL, String USER, String PASS)  {
@@ -44,15 +71,45 @@ public class DiscordDAO {
     public static <T> int insert(T object)  {
         if (object instanceof DiscordUser)  {
             DiscordUser user = (DiscordUser) object;
-
             try  {
-                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (username, level, country, timezone) VALUES (?, ?, ?, ?)");
-                preparedStatement.setString(1, user.getName());
-                preparedStatement.setString(2, user.getLevel());
-                preparedStatement.setString(3, user.getCountry());
-                preparedStatement.setString(4, user.getTimezone());
+                int id = getIdByColumn(object, Column.IDSTRING);
+                if (id<=0)  {
+                    System.out.println("storing user " + user.getName());
+                    PreparedStatement createUser  = connection.prepareStatement("INSERT INTO " +
+                            "users ("+ Column.IDSTRING.databaseLabel + ") VALUES (?);");
+                    createUser.setString(1, user.getId());
 
-                return preparedStatement.executeUpdate();
+                    createUser.executeUpdate();
+                }
+
+                PreparedStatement columnsStatement = connection.prepareStatement("SELECT * FROM users WHERE false;");
+                ResultSet columns = columnsStatement.executeQuery();
+                //columns.next();
+
+                int result = 0;
+                for (int i=1; i<=columns.getMetaData().getColumnCount(); i++)  {
+                    String column = columns.getMetaData().getColumnName(i);
+                    System.out.println("column: " + column);
+                    if (user.getAnswered().keySet().contains(column))  {
+                        PreparedStatement updateTable = connection.prepareStatement("UPDATE users SET (" + column +
+                                    ") = (?) WHERE " + Column.IDSTRING.databaseLabel + "='" + user.getId() + "';");
+                        updateTable.setObject(1, user.getAnswered().get(column).getAnswer());
+
+                        result = updateTable.executeUpdate();
+                    }
+                }
+
+                System.out.println("STORING TAG: " + user.getTag());
+                PreparedStatement updateOther = connection.prepareStatement("UPDATE users " +
+                        "SET (" + Column.USERNAME.databaseLabel + ", " +
+                        Column.TAGSTRING.databaseLabel +") = " +
+                        "(?, ?) WHERE " + Column.IDSTRING.databaseLabel + "='" + user.getId() + "';");
+                updateOther.setString(1, user.getName());
+                updateOther.setString(2, user.getTag());
+
+                updateOther.executeUpdate();
+
+                return result;
             }
             catch (Exception e)  {
                 e.printStackTrace();
@@ -60,6 +117,116 @@ public class DiscordDAO {
             }
         }
 
+        System.out.println("not a discord user");
+
         return -2;
     }
+
+    public static <T> T getByUsername(Class type, String username)  {
+       System.out.println("inside getByUsername: " + username);
+
+       return getById(type, getIdByColumn(username, Column.USERNAME));
+    }
+
+    public static <T> T getByIdString(Class type, String idString)  {
+       return getById(type, getIdByColumn(idString, Column.IDSTRING));
+    }
+
+    public static <T> T getByTagString(Class type, String tagString)  {
+       System.out.println("THIS TAG STRING" + tagString);
+
+       boolean isId = false;
+       if (tagString.charAt(0)=='<')  {
+           isId = true;
+           tagString = tagString.substring(1, tagString.length()-1);
+       }
+
+       if (tagString.charAt(0)=='@')  {
+            System.out.println("@@@");
+            tagString = tagString.substring(1);
+        }
+
+        if (isId)  {
+            return getById(type, getIdByColumn(tagString, Column.IDSTRING));
+        }
+
+        return getById(type, getIdByColumn(tagString, Column.TAGSTRING));
+    }
+
+    public static <T> T getById(Class type, int id)  {
+       System.out.println("Inside getById(" + type + ", " + id + ")");
+
+        if (type == DiscordUser.class)  {
+            try  {
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users " +
+                        "WHERE id=" + id);
+                ResultSet result = preparedStatement.executeQuery();
+                if (!result.next())  {
+                    System.out.println("empty");
+                    return null;
+                }
+
+                DiscordUser user = new DiscordUser();
+                for (int i=1; i<=result.getMetaData().getColumnCount(); i++)  {
+                    if (result.getString(i)!=null && !result.getString(i).equalsIgnoreCase(""))  {
+                        System.out.println("i: " + i);
+                        System.out.println(result.getMetaData().getColumnName(i));
+                        System.out.println(result.getString(i));
+
+                        if (Column.getByDatabaseLabel(result.getMetaData().getColumnName(i))!=null) {
+                            user.set(Column.getByDatabaseLabel(result.getMetaData().getColumnName(i)).userLabel, result.getString(i));
+                        }
+                    }
+                }
+                user.setTag(result.getString(Column.TAGSTRING.databaseLabel));
+
+               /* user.setName(result.getString("username"));
+                user.setTimezone(result.getString("timezone"));
+                user.setLevel(result.getString("level"));
+                user.setAge(result.getInt("age"));
+                user.setCountry(result.getString("country"));*/
+
+                return (T) user;
+            } catch (Exception e)  {
+                    e.printStackTrace();
+                    return null;
+            }
+        }
+        else  {
+            return null;
+        }
+    }
+
+    public static <T> int getIdByColumn(T object, Column column)  {
+            Object value;
+
+            if (object instanceof DiscordUser) {
+                value = ((DiscordUser) object).get(column.userLabel);
+            }
+            else if (object instanceof String) {
+                value = (String) object;
+            }
+            else {
+                return 0;
+            }
+
+           try {
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT id FROM users " +
+                        "WHERE " + column.databaseLabel + "='" + value + "' ORDER BY id ASC");
+                System.out.println("frodo databaselevel: " + column.databaseLabel);
+                ResultSet result = preparedStatement.executeQuery();
+                int objectId;
+                if (result.next()) {
+                    objectId = result.getInt("id");
+                } else {
+                    objectId = 0;
+                }
+
+                return objectId;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return -1;
+            }
+    }
 }
+
--- src/main/java/javastudybuddies/discordbots/DiscordUser.java
+++ src/main/java/javastudybuddies/discordbots/DiscordUser.java
@@ -1,26 +1,115 @@
 package javastudybuddies.discordbots;
 
-import java.util.ArrayList;
-import java.util.List;
+import javastudybuddies.discordbots.welcomebot.Answer;
+
+import java.util.*;
 
 public class DiscordUser {
-        private String username;
-        private String level;
-        private String country;
-        private String timezone;
+        Map<String, Answer<?>> unanswered;
+        Map<String, Answer<?>> answered;
 
+        private String id;
+        private String tag;
+
+        private String chatId;
         private ArrayList<LeetcodeTask> solved;
 
+        public DiscordUser()  {
+                unanswered = new HashMap<>();
+                answered = new HashMap<>();
+
+                unanswered.put("username", null);
+                unanswered.put("level", null);
+                unanswered.put("country", null);
+                unanswered.put("timezone", null);
+                unanswered.put("age", null);
+                unanswered.put("goal", null);
+                unanswered.put("tech", null);
+        }
+
+        public void answer(String question, Answer<?> answer)  {
+                if (unanswered.keySet().contains(question))  {
+                    unanswered.remove(question);
+                }
+
+                answered.put(question, answer);
+        }
+
+        public boolean allAnswered()  {return unanswered.size()==0;}
+        public Set<String> questionsToAnswer()  {return unanswered.keySet();}
+
+    public static void main(String[] args) {
+        DiscordUser user = new DiscordUser();
+        user.set("username", "someone");
+        System.out.println(user.getName());
+    }
+
         //getters
-        public String getName() {return username;}
+        public String getName() {return (String) answered.get("username").getAnswer();}
         public List<LeetcodeTask> getLeetcodeTasks()  {return solved;}
-        public String getLevel()  {return level;}
-        public String getCountry()  {return country;}
-        public String getTimezone()  {return timezone;}
+        public String getLevel()  {return (String) answered.get("level").getAnswer();}
+        public String getCountry()  {return (String) answered.get("country").getAnswer();}
+        public String getTimezone()  {return (String) answered.get("timezone").getAnswer();}
+        public String getChatId()  {return chatId;}
+        public int getAge() {return (Integer) answered.get("age").getAnswer();}
+        public String getId()  {return id;}
+        public String getTag()  {return tag;}
+        public String getGoal()  {return (String) answered.get("goal").getAnswer();}
+        public String getTech()  {return (String) answered.get("tech").getAnswer();}
+
+        public Map<String, Answer<?>> getAnswered()  {return answered;}
 
         //setters
-        public void setName(String username)  {this.username = username;}
-        public void setLevel(String level)  {this.level = level;}
-        public void setCountry(String country)  {this.country = country;}
-        public void setTimezone(String timezone)  {this.timezone = timezone;}
+        public void setTech(String tech)  {this.answer("tech", new Answer<>("tech", tech));}
+        public void setGoal(String goal)  {this.answer("goal", new Answer<>("goal", goal));}
+        public void setTag(String tag)  {this.tag = tag; }
+        public void setId(String id)  {this.id = id;}
+        public void setName(String username)  {this.answer("username",
+                                                                new Answer("username", username));}
+        public void setLevel(String level)  {this.answer("level",
+                                                                new Answer("level", level));}
+        public void setCountry(String country)  {this.answer("country",
+                                                                new Answer("country", country));}
+        public void setTimezone(String timezone)  {this.answer("timezone",
+                                                                new Answer("timezone", timezone));}
+        public void setChatId(String chatId)  {this.chatId = chatId;}
+        public void setAge(int age)  {this.answer("age", new Answer("age", age));}
+
+        public void set(String type, String answer)  {
+            switch (type)  {
+                case "level":
+                    setLevel(answer);
+                    break;
+                case "age":
+                    setAge(Integer.parseInt(answer));
+                    break;
+                case "timezone":
+                    setTimezone(answer);
+                    break;
+                case "country":
+                    setCountry(answer);
+                    break;
+                case "username":
+                    setName(answer);
+                    break;
+                case "goal":
+                    setGoal(answer);
+                case "tech":
+                    setTech(answer);
+                default:
+                    System.out.println("No such type: " + type);
+            }
+        }
+
+        public Object get(String type)  {
+            if (type.equalsIgnoreCase("id"))  {
+                return id;
+            }
+
+            if (type.equalsIgnoreCase("tag"))  {
+                return tag;
+            }
+
+            return answered.get(type);
+        }
 }
